c1b34eb187a81d509785cd28639b9a5a4cc978a8
Arianna Cipolla <ariannacipolla2002@gmail.com>
2024-04-19 18:44:36 +0200

    @ariannacipolla: make subgraphs

--- src/it/unipr/informatica/tirocin/Main.java
+++ src/it/unipr/informatica/tirocin/Main.java
@@ -22,16 +22,24 @@ public class Main {
 			list.add(new Graph("gruppo1"+File.separator+f.getName()+File.separator+"nodes_graph.json",
 								"gruppo1"+File.separator+f.getName()+File.separator+"edges_graph.json"));
 			fnames.add(f.getName());
-			System.out.println("added"+ c++);
+			System.out.println("added: "+ c++);
 		}		
+		
 		c = 0;
-		int count = 0 ;
-		for(Graph g:list) {
-			try(FileOutputStream fos = new FileOutputStream("outs_group1"+File.separator+ fnames.get(count++) +".dot");
-				PrintStream ps = new PrintStream(fos);) {
-				g.printGraph(ps,false);	
-			}
-			System.out.println("added"+ c++);
+		for (int i = 0; i < list.size(); i++) {
+		    Graph g = list.get(i);
+		    String fname = fnames.get(i);
+		    
+		    List<Graph> subgraphs = g.subgraphs(3, 3);
+		    
+			try (FileOutputStream fos1 = new FileOutputStream("outs_group1" + File.separator + fname + ".dot");
+				     PrintStream ps1 = new PrintStream(fos1);
+				     FileOutputStream fos2 = new FileOutputStream("outs_subgraphs" + File.separator + fname + "_combined.dot");
+				     PrintStream ps2 = new PrintStream(fos2)) {
+				    g.printGraph(ps1, false);
+				    g.printMultipleGraphs(ps2, subgraphs);
+				}
+			System.out.println("dimensione: "+ subgraphs.size());
 		}   
 		long tempo2=System.currentTimeMillis();
 		long tempone = tempo2-tempo1;
