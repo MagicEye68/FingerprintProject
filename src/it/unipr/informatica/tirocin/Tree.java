c0f34948f3afaeb66ae62555b183c52a3baf352b
MagicEye68 <corazzini.marco@gmail.com>
2024-07-17 21:50:39 +0200

    add dfs tree

---
+++ src/it/unipr/informatica/tirocin/Tree.java
@@ -0,0 +1,27 @@
+package it.unipr.informatica.tirocin;
+
+public class Tree {
+	private TreeNode root;
+	public Tree(TreeNode root) {
+		this.root = root;
+	}
+	
+	public boolean isomorph(Tree other) {
+		if(root.children.size() != other.root.children.size()) {
+			return false;
+		}
+		if(!root.type.equals(other.root.type)) {
+			return false;
+		}
+		for(int i = 0; i < root.children.size(); i++) {
+			Tree t1 =new Tree(root.children.get(i));
+			Tree t2 =new Tree(other.root.children.get(i));
+			if(!t1.isomorph(t2)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+
+}
---
+++ src/it/unipr/informatica/tirocin/TreeNode.java
@@ -0,0 +1,26 @@
+package it.unipr.informatica.tirocin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TreeNode{
+	private int[] coordinates;
+	public String type;
+	public List<TreeNode> children;
+	
+	public TreeNode(int [] coordinates, String type) {
+		this.coordinates = coordinates;
+		this.type=type;
+		children = new ArrayList<>();
+	}
+	
+	public void addChild(TreeNode child) {
+		children.add(children.size(),child);
+	}
+
+	
+	@Override
+	public String toString() {
+		return "x: "+coordinates[0]+" y: "+coordinates[1]+" ,type: "+type;
+	}	
+}
